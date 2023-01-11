56a0d42f9bce9a198c2742284dd9b958d1a1fabc
VCarbonell <v.carbonell+gh@hotmail.fr>
2023-01-11 15:24:50 +0100

    car routes done

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const userRouter = require("./userRouter");
+const carRouter = require("./carRouter");
 
 const router = express.Router();
 
 router.use("/users", userRouter);
+router.use("/car", carRouter);
 
 module.exports = router;
