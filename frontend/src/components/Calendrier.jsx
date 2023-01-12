c862f06ea8f8ba755c0d49f6320678b94f9da0fe
PeterFlandin <peterflandin@gmail.com>
2023-01-12 14:01:10 +0100

    essaie

--- frontend/src/components/Calendrier.jsx
+++ frontend/src/components/Calendrier.jsx
@@ -2,33 +2,24 @@ import { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import "./Calendrier.scss";
+import Singlerange from "./SingleRange";
 
 function Calendrier() {
   const [date, setDate] = useState(new Date());
-
   return (
-    <div className="app">
+    <div className="calendar">
+      <div className="title">
+        <h1>CHOOSE YOUR DATES</h1>
+      </div>
       <div className="calendar-container">
         <Calendar
           onChange={setDate}
           value={date}
-          selectRange={true}
+          selectRange
           minDate={new Date()}
-          maxDate={new Date(2023, 12, 31)}
+          minDetail="month"
         />
       </div>
-      {date.length > 0 ? (
-        <p className="text-center">
-          <span className="bold">Start:</span> {date[0].toDateString()}
-          &nbsp;|&nbsp;
-          <span className="bold">End:</span> {date[1].toDateString()}
-        </p>
-      ) : (
-        <p className="text-center">
-          <span className="bold">Default selected date:</span>{" "}
-          {date.toDateString()}
-        </p>
-      )}
     </div>
   );
 }
